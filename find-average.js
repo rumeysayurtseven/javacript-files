f6a38d3a3fd32772695441db827c5b7fc38aea95
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2020-09-13 23:49:24 +0300

    Add files via upload

---
+++ find-average.js
@@ -0,0 +1,14 @@
+var grades=[2,5, , ,9,8, ,8];
+		var sum=0;
+		var count=0;
+		if(grades.length>0){
+			for(index=0; index < grades.length; index++){
+				if(grades[index] !=undefined){
+					sum+=grades[index];
+					count=count+1;
+				}
+			}		
+			document.write(sum/count);
+	}
+	else 
+        document.write("Empty Array");
