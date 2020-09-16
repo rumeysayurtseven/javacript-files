02557132f4c02a04c36bb77e4ea685698cc45526
Rumeysa Yurtseven <70800710+rumeysayurtseven@users.noreply.github.com>
2020-09-16 21:15:29 +0300

    Add files via upload

---
+++ FizzBuzz Project from Odin/FizzBuzz.js
@@ -0,0 +1,29 @@
+let n =parseInt (prompt("Pick a number"));
+for(let i=1; i<=n; i++){
+    if(i%3===0 && i%5===0){
+        console.log("FizzBuzz");   
+     }
+        else if(i%3===0){
+        console.log("Fizz");
+    }
+        else if(i%5===0){
+        console.log("Buzz");  
+      }
+        else{
+    console.log(i);
+    }
+}
+
+// let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+// for (let i = 1; i <= answer; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
